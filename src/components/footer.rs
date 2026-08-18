195dad01a5f5093dc10ab1699be4fd52c9e34c15
Sonicka Jadeesh <317060254+sonickajadeesh@users.noreply.github.com>
2026-08-18 18:13:36 +0530

    misc: adjust vertical padding

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -3,7 +3,7 @@ use dioxus::prelude::*;
 #[component]
 pub fn Footer() -> Element {
     rsx! {
-        footer { class: "border-t border-gray-200 px-4 py-2.5 text-center text-xs leading-relaxed text-gray-500",
+        footer { class: "border-t border-gray-200 px-4 py-2 text-center text-xs leading-relaxed text-gray-500",
 
             p { "Disclaimer: This is not intended to provide professional medical consultation." }
         }
